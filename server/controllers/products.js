541a0f0d7d5642ded43af832b02249be7e0b9d5d
KolliSai <saik98187@gmail.com>
2023-06-10 21:53:58 +0530

    step4 completed

--- server/controllers/products.js
+++ server/controllers/products.js
@@ -9,4 +9,38 @@ const addMultipleProducts = async (req, res) => {
   }
 };
 
-module.exports = { addMultipleProducts };
+const getAllProducts = async (req, res) => {
+  try {
+    let products = await Product.find({});
+    res.status(200).json({ data: products });
+  } catch (error) {
+    res.json({ err: `error in getAllProducts -> ${error}` });
+  }
+};
+
+const getProduct = async (req, res) => {
+  try {
+    let product = await Product.findOne({ _id: req.params.id });
+    res.status(200).json({ data: product });
+  } catch (error) {
+    res.json({ err: `error in getProduct -> ${error}` });
+  }
+};
+
+const getProductByCategory = async (req, res) => {
+  const { category } = req.params;
+  console.log(category);
+  try {
+    let product = await Product.find({ category });
+    res.status(200).json({ data: product });
+  } catch (error) {
+    res.json({ err: `error in getProductByCategory -> ${error}` });
+  }
+};
+
+module.exports = {
+  addMultipleProducts,
+  getAllProducts,
+  getProduct,
+  getProductByCategory,
+};
--- server/routes/products.js
+++ server/routes/products.js
@@ -1,7 +1,15 @@
 const express = require("express");
-const { addMultipleProducts } = require("../controllers/products");
+const {
+  addMultipleProducts,
+  getAllProducts,
+  getProduct,
+  getProductByCategory,
+} = require("../controllers/products");
 const router = express.Router();
 
 router.post("/products", addMultipleProducts);
+router.get("/products", getAllProducts);
+router.get("/product/:id", getProduct);
+router.get("/products/:category", getProductByCategory);
 
 module.exports = router;
